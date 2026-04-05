17a22e60ad8034ccb3f4d081843c9fc185c9f639
Vinaya kumara C <vk544114@gmail.com>
2026-04-05 23:48:45 +0530

    Added JWT authentication middleware

---
+++ youtube-Backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    // Remove Bearer if exists
+    if (token.startsWith("Bearer ")) {
+      token = token.replace("Bearer ", "");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.id;
+
+    next();
+
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
