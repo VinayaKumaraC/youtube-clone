19ce931f334feb5f2c4124eac666b0d4a34fb699
Vinaya kumara C <vk544114@gmail.com>
2026-04-06 00:15:30 +0530

    Added create video API

---
+++ youtube-Backend/controllers/videoController.js
@@ -0,0 +1,29 @@
+import Video from "../models/Video.js";
+
+// Create video
+export const createVideo = async (req, res) => {
+  try {
+    const { title, description, url } = req.body;
+
+    if (!title || !url) {
+      return res.status(400).json({
+        message: "Title and URL are required",
+      });
+    }
+
+    const video = await Video.create({
+      title,
+      description,
+      url,
+      user: req.user, // from middleware
+    });
+
+    res.status(201).json({
+      message: "Video created successfully",
+      video,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
