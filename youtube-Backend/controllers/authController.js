21bf2f580b1b8ff751fa3aabb66b5ec82ce2ec35
Vinaya kumara C <vk544114@gmail.com>
2026-04-05 23:44:28 +0530

    Implemented login API with JWT authentication

--- youtube-Backend/controllers/authController.js
+++ youtube-Backend/controllers/authController.js
@@ -7,13 +7,13 @@ export const registerUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // 🔥 Email validation
+    // Email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    // 🔥 Password validation
+    // Password validation
     if (password.length < 6) {
       return res.status(400).json({
         message: "Password must be at least 6 characters",
@@ -25,7 +25,7 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // 🔐 Hash password
+    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
@@ -38,6 +38,31 @@ export const registerUser = async (req, res) => {
       _id: user._id,
       email: user.email,
     });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET
+      );
+
+      res.json({
+        message: "Login successful",
+        token,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
 
   } catch (error) {
     res.status(500).json({ message: error.message });
