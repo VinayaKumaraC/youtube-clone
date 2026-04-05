4c1a846892033f078744b7e96aac7f3b8374550e
Vinaya kumara C <vk544114@gmail.com>
2026-04-06 00:21:47 +0530

    Connected video routes to server

--- youtube-Backend/server.js
+++ youtube-Backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
 
 
 dotenv.config();
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
 
 // Test route
 app.get("/", (req, res) => {
