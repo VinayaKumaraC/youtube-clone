a39732f99ced5273d3541f7378e55ca13a965d17
Vinaya kumara C <vk544114@gmail.com>
2026-04-05 23:47:52 +0530

    Connected auth routes to server

--- youtube-Backend/server.js
+++ youtube-Backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
 
 dotenv.config();
 connectDB();
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
