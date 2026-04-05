392563d3b3578605e6d79b63a11c597b6e67537a
Vinaya kumara C <vk544114@gmail.com>
2026-04-06 00:17:34 +0530

    Implemented get all videos API

--- youtube-Backend/controllers/videoController.js
+++ youtube-Backend/controllers/videoController.js
@@ -27,3 +27,15 @@ export const createVideo = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get all videos
+export const getAllVideos = async (req, res) => {
+  try {
+    const videos = await Video.find().populate("user", "name");
+
+    res.json(videos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
