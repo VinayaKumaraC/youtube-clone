59919d54da61f9590229a26e748dc66f06004dde
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:33:44 +0530

    Added like/dislike fields in video schema

--- youtube-Backend/models/Video.js
+++ youtube-Backend/models/Video.js
@@ -5,12 +5,25 @@ const videoSchema = new mongoose.Schema(
     title: String,
     description: String,
     url: String,
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+
+    // likes for video
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    // dislikes for video
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
 export default mongoose.model("Video", videoSchema);
