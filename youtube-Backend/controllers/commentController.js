e0bebb34fe58b8fc7fc82ebdabc88d9b31c084bb
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:25:53 +0530

    Added delete comment functionality with authorization

--- youtube-Backend/controllers/commentController.js
+++ youtube-Backend/controllers/commentController.js
@@ -40,3 +40,32 @@ export const getCommentsByVideo = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Delete comment
+export const deleteComment = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      return res.status(404).json({
+        message: "Comment not found",
+      });
+    }
+
+    // Only login user can delete
+    if (comment.user.toString() !== req.user) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    await comment.deleteOne();
+
+    res.json({
+      message: "Comment deleted successfully",
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
