d2208b4b3e39f1b77fb5da884859e2cd66d6ea23
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:35:01 +0530

    Added like video functionality

--- youtube-Backend/controllers/videoController.js
+++ youtube-Backend/controllers/videoController.js
@@ -110,3 +110,25 @@ export const deleteVideo = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Like for  video 
+export const likeVideo = async (req, res) => {
+  try {
+    const video = await Video.findById(req.params.id);
+
+    if (!video) {
+      return res.status(404).json({ message: "Video not found" });
+    }
+
+    video.likes += 1;
+    await video.save();
+
+    res.json({
+      message: "Video liked",
+      likes: video.likes,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
