3f13d3841b00fe42f8ce4c7c9967d62537d221c9
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:27:50 +0530

    Connected comment routes to server

--- youtube-Backend/server.js
+++ youtube-Backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 // Test route
 app.get("/", (req, res) => {
