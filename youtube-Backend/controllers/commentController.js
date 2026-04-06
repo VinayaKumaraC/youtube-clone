7e271ef7a8121d7d486453b8c9ed2005e44eb08f
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:25:03 +0530

    Implemented fetch comments by video

--- youtube-Backend/controllers/commentController.js
+++ youtube-Backend/controllers/commentController.js
@@ -26,3 +26,17 @@ export const addComment = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get comments by video
+export const getCommentsByVideo = async (req, res) => {
+  try {
+    const comments = await Comment.find({
+      video: req.params.videoId,
+    }).populate("user", "name");
+
+    res.json(comments);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
