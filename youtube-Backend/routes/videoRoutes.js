373d1611d55e4b12ccd7e393ecf80dc7e02300c5
Vinaya kumara C <vk544114@gmail.com>
2026-04-07 02:38:26 +0530

    Integrated like and dislike routes

--- youtube-Backend/routes/videoRoutes.js
+++ youtube-Backend/routes/videoRoutes.js
@@ -7,6 +7,7 @@ import {
   deleteVideo,
 } from "../controllers/videoController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import { likeVideo, dislikeVideo } from "../controllers/videoController.js";
 
 const router = express.Router();
 
@@ -15,5 +16,7 @@ router.get("/", getAllVideos);
 router.get("/:id", getVideoById);
 router.put("/:id", authMiddleware, updateVideo);
 router.delete("/:id", authMiddleware, deleteVideo);
+router.put("/:id/like", authMiddleware, likeVideo);
+router.put("/:id/dislike", authMiddleware, dislikeVideo);
 
 export default router;
