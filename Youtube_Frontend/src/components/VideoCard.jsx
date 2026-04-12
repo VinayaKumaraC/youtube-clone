91b74bba80f77ccb224c8af6956fd97d88ace37d
Vinaya kumara C <vk544114@gmail.com>
2026-04-12 17:55:45 +0530

    Ui fix

--- Youtube_Frontend/src/components/VideoCard.jsx
+++ Youtube_Frontend/src/components/VideoCard.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 function VideoCard({ video }) {
   return (
-    <Link to={`/video/${video._id}`}>
-      <div>
-        <img src={video.thumbnail || "https://via.placeholder.com/200"} width="200" />
-        <h4>{video.title}</h4>
-        <p>{video.channel?.channelName}</p>
-        <p>{video.views} views</p>
-      </div>
-    </Link>
+    <div style={{ cursor: "pointer" }}>
+      <img
+        src={video.thumbnail || "https://via.placeholder.com/300x180"}
+        style={{ width: "100%", borderRadius: "10px" }}
+      />
+
+      <h4 style={{ margin: "5px 0" }}>{video.title}</h4>
+      <p style={{ color: "gray" }}>{video.channel?.channelName}</p>
+      <p style={{ color: "gray" }}>{video.views} views</p>
+    </div>
   );
 }
 
