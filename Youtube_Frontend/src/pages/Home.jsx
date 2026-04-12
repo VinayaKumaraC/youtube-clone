679310224912529e458b5e781198353f015e028e
Vinaya kumara C <vk544114@gmail.com>
2026-04-12 17:54:57 +0530

    update home .jsx

--- Youtube_Frontend/src/pages/Home.jsx
+++ Youtube_Frontend/src/pages/Home.jsx
@@ -1,29 +1,43 @@
-// displays videos + search + filters
-
 import { useEffect, useState } from "react";
 import API from "../api/axios";
 import VideoCard from "../components/VideoCard";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
   const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
 
   const fetchVideos = async () => {
-    const res = await API.get("/videos");
+    const res = await API.get(`/videos?search=${search}&category=${category}`);
     setVideos(res.data);
   };
 
   useEffect(() => {
     fetchVideos();
-  }, []);
+  }, [search, category]);
 
   return (
     <div>
-      <h2>Home</h2>
+      <Header setSearch={setSearch} />
 
-      <div style={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", gap: "10px" }}>
-        {videos.map((v) => (
-          <VideoCard key={v._id} video={v} />
-        ))}
+      <div style={{ display: "flex" }}>
+        <Sidebar setCategory={setCategory} />
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: "20px",
+            padding: "20px",
+            flex: 1,
+          }}
+        >
+          {videos.map((v) => (
+            <VideoCard key={v._id} video={v} />
+          ))}
+        </div>
       </div>
     </div>
   );
