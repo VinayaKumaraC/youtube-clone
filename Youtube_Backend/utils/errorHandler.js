5287c8db427bd9b6e46dd9e9a2639ffd7bf8a32b
Vinaya kumara C <vk544114@gmail.com>
2026-04-12 19:42:35 +0530

    update utils/error handler

--- Youtube_Backend/utils/errorHandler.js
+++ Youtube_Backend/utils/errorHandler.js
@@ -5,6 +5,7 @@ const errorHandler = (err, req, res, next) => {
   console.error("Error:", err.message);
 
   res.status(err.statusCode || 500).json({
+    success: false,
     message: err.message || "Server Error",
   });
 };
