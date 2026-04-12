d98472ea704f1c840b8302c5a93a52d980cf736f
Vinaya kumara C <vk544114@gmail.com>
2026-04-12 15:34:41 +0530

    Setup server with all routes

---
+++ server.js
@@ -0,0 +1,33 @@
+// main entry point
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/channels", channelRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server started");
+});
