0bb2fb65f3cf83ff7e5ffd52bc1946b264d48042
Vinaya kumara C <vk544114@gmail.com>
2026-04-12 17:30:35 +0530

    Setup React entry point with AuthProvider

--- Youtube_Frontend/src/main.jsx
+++ Youtube_Frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// entry point of react app
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
